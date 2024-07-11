5a84ebf3ff05b86c366f2f451550b4c5e47faa22
Md-Alqma <mdalqma7@gmail.com>
2024-07-11 09:54:34 +0530

    added animation in the intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 const Intro = () => {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -41,13 +41,16 @@ const Intro = () => {
           </motion.span>
         </div>
       </div>
-      <p>
+      <motion.p
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        className=" mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
         <span className="font-bold">Hello, I'm Alqma.</span> I'm a{" "}
         <span className="font-bold">Full-Stack Web Developer</span> with{" "}
         <span className="font-bold">2+ years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-      </p>
+      </motion.p>
     </section>
   );
 };
