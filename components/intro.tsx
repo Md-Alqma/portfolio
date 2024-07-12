f5513f4a2bf8d454c0e8a0fbaf5869d54743f84f
Md-Alqma <mdalqma7@gmail.com>
2024-07-12 18:31:11 +0530

    Created the action buttons

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,10 +3,14 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 const Intro = () => {
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+    <section className="z-0 mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -51,6 +55,35 @@ const Intro = () => {
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.p>
+
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+        className="flex flex-col items-center justify-center gap-2 px-4 text-lg font-medium sm:flex-row ">
+        <Link
+          href="#contact"
+          className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">
+          Contact me here <BsArrowRight />
+        </Link>
+        <a
+          href=""
+          className="bg-white px-7 py-3 flex items-center gap-2 rounded-full ">
+          Download CV <HiDownload />
+        </a>
+        <a
+          href=""
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full">
+          <BsLinkedin />
+        </a>
+        <a
+          href=""
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full">
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 };
