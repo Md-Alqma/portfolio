68d8c0a958b2cd33f2566f374a4f65e466454dba
Md-Alqma <mdalqma7@gmail.com>
2024-07-15 12:51:39 +0530

    created a section divider after intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -10,7 +10,7 @@ import { FaGithubSquare } from "react-icons/fa";
 
 const Intro = () => {
   return (
-    <section className="z-0 mb-28 max-w-[50rem] text-center sm:mb-0">
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -22,13 +22,12 @@ const Intro = () => {
             }}>
             <Image
               src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Profile Photo"
+              alt="Ricardo portrait"
               width="192"
               height="192"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover
-            border-[0.35rem] border-white shadow-xl"
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
           <motion.span
@@ -65,22 +64,27 @@ const Intro = () => {
         className="flex flex-col items-center justify-center gap-2 px-4 text-lg font-medium sm:flex-row ">
         <Link
           href="#contact"
-          className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">
-          Contact me here <BsArrowRight />
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 hover:bg-gray-950 active:scale-105 transition">
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-2 transition" />
         </Link>
         <a
-          href=""
-          className="bg-white px-7 py-3 flex items-center gap-2 rounded-full ">
-          Download CV <HiDownload />
+          href="/CV.pdf"
+          download
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10">
+          Download CV{" "}
+          <HiDownload className="opacity-70 group-hover:translate-y-1 transition" />
         </a>
         <a
-          href=""
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full">
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://linkedin.com/in/mdalqma"
+          target="_blank">
           <BsLinkedin />
         </a>
         <a
-          href=""
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full">
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/Md-Alqma"
+          target="_blank">
           <FaGithubSquare />
         </a>
       </motion.div>
